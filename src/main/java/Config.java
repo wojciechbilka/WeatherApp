c2ba88f37a86a4fbc8b0e1479b4afa9dadc93168
Wojciech Biłka <wojciech.bilka@gmail.com>
2020-04-22 15:46:55 +0200

    Added Config class

---
+++ src/main/java/Config.java
@@ -0,0 +1,6 @@
+public class Config {
+    public static final String APP_ID = "7e4319f8d58e8ad9df392624485bff10";
+    public static final String APP_URL = "http://api.openweathermap.org/data/2.5/weather";
+    public static final String UNITS = "metric";
+    public static final String LANG = "pl";
+}
