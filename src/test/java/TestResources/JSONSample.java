dae35b263a3ff00ec7b3906e513dfb97af47a86c
Wojciech Biłka <wojciech.bilka@gmail.com>
2020-04-23 12:21:58 +0200

    Added Sample JSON String in JSONSample class.

---
+++ src/test/java/TestResources/JSONSample.java
@@ -0,0 +1,5 @@
+package TestResources;
+
+public class JSONSample {
+    public static final String sample = "{\"coord\":{\"lon\":21.01,\"lat\":52.23},\"weather\":[{\"id\":803,\"main\":\"Clouds\",\"description\":\"pochmurno z przejaśnieniami\",\"icon\":\"04d\"}],\"base\":\"stations\",\"main\":{\"temp\":15.45,\"feels_like\":10.9,\"temp_min\":15,\"temp_max\":16.11,\"pressure\":1023,\"humidity\":28},\"visibility\":10000,\"wind\":{\"speed\":3.1,\"deg\":310},\"clouds\":{\"all\":75},\"dt\":1587631498,\"sys\":{\"type\":1,\"id\":1713,\"country\":\"PL\",\"sunrise\":1587612046,\"sunset\":1587664039},\"timezone\":7200,\"id\":756135,\"name\":\"Warszawa\",\"cod\":200}";
+}
